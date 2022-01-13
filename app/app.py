12a8efee7295405391f9af953aaa7a667d81cac9
hiro9108 <hiroshi.8.egawa@gmail.com>
2022-01-12 21:35:11 -0800

    Add sytles.css

--- app/app.py
+++ app/app.py
@@ -1,24 +1,32 @@
-from flask import Flask,render_template,request
+from flask import Flask,redirect,render_template,request
 from bs4 import BeautifulSoup
 import requests
 
 app = Flask(__name__)
 
-soup =BeautifulSoup
+soup = BeautifulSoup
 
 @app.route("/")
 def index():
-    return "Please add /load_title?url=https://google.com to your URL for example"
+    return render_template("index.html", title="Please add your query parameter to URL. Default is load_title?url=https://google.com")
 
 @app.route("/load_title")
 def load_title():
-    url = request.args.get("url")
-    res = requests.get(url)
-    soup = BeautifulSoup(res.text, "html.parser")
-    result = soup.title.string
+    try:
+        url = request.args.get("url")
 
-    print("--- Output ---")
-    print("Title: ", result)
-    print("--------------")
+        res = requests.get(url)
+        soup = BeautifulSoup(res.text, "html.parser")
+        result = soup.title.string
 
-    return render_template("index.html", title=result)
+        print("--- Output ---")
+        print("Title: ", result)
+        print("--------------")
+
+        return render_template("index.html", title=result)
+    except:
+        return redirect("/load_title?url=https://google.com")
+  
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('error.html', title='The requested URL was Not found')
